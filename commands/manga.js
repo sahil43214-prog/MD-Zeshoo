c43853a17dd29972e6e752d4d296ebe9e35cd68a
sahil43214-prog <sahil43214@gmail.com>
2026-09-11 01:58:41 +0530

    Update manga.js

--- commands/manga.js
+++ commands/manga.js
@@ -6,7 +6,7 @@ module.exports = async function(sock, chatId, msg, q) {
     try {
         await sock.sendMessage(chatId, { text: '\u1F4D6 Searching manga...' }, { quoted: msg });
         
-        const response = await axios.get(`https://api.jikan.moe/v4/manga?q=${encodeURIComponent(q)}&limit=3`, { timeout: 10000 });
+        const response = await axios.get(`https://api.jikan.moe/v4/manga?q=${encodeURIComponent(q)}&limit=3`, { timeout: 7000 });
         const mangas = response.data.data;
         
         if (!mangas || !mangas.length) return await sock.sendMessage(chatId, { text: '\u274C No manga found!' }, { quoted: msg });
